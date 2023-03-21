2287702fee8e3025f2fbe1b0fc581055318c94f2
jumperM4 <pma1994ars@yandex.ru>
2023-03-21 21:28:39 +0300

    some changes

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-let button = document.getElementById("enter");
-let input = document.getElementById("userinput");
-let ul = document.querySelector("ul");
+const button = document.getElementById("enter");
+const input = document.getElementById("userinput");
+const ul = document.querySelector("ul");
 
-function inputLength() {
+function getInputLength() {
   return input.value.length;
 }
 
 function createListElement() {
-  let li = document.createElement("li");
+  const li = document.createElement("li");
   li.className = "listItem";
   li.appendChild(document.createTextNode(input.value));
-  li.addEventListener("click", toggleDone);
+  li.addEventListener("click", () => li.classList.toggle("done"));
   ul.appendChild(li);
   input.value = "";
 
-  let buttonDelete = document.createElement("button");
+  const buttonDelete = document.createElement("button");
   buttonDelete.innerText = "Delete";
   buttonDelete.className = "delBtn";
-  buttonDelete.addEventListener("click", deleteItem);
+  buttonDelete.addEventListener("click", () => li.remove());
   li.append(buttonDelete);
 
-  function toggleDone() {
-    li.classList.toggle("done");
-  }
+  // function toggleDone() {
+  //   li.classList.toggle("done");
+  // }
 
-  function deleteItem() {
-    li.remove();
-  }
+  // function deleteItem() {
+  //   li.remove();
+  // }
 }
 
 function addListItemAfterClick() {
-  if (inputLength() > 0) {
+  if (getInputLength() > 0) {
     createListElement();
   }
 }
 
 function addListItemAfterPress(event) {
-  if (inputLength() > 0 && event.keyCode === 13) {
+  if (getInputLength() > 0 && event.which === 13) {
     createListElement();
   }
 }
