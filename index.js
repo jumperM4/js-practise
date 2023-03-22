98f7a754666aa56646bfc1957238b70fd8b1b247
jumperM4 <pma1994ars@yandex.ru>
2023-03-22 21:58:51 +0300

    rendering product cards

--- index.js
+++ index.js
@@ -1,15 +1,95 @@
-const css = document.querySelector("h3");
-const color1 = document.querySelector(".color1");
-const color2 = document.querySelector(".color2");
-const body = document.getElementById("gradient");
+const productsDiv = document.createElement("div");
+productsDiv.className = ".products";
+document.body.append(productsDiv);
 
-function setGradient() {
-  body.style.background =
-    "linear-gradient(to right," + color1.value + ", " + color2.value + ")";
+const productItemInfo = [
+  {
+    id: 1,
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109.95,
+    description:
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    rating: { rate: 3.9, count: 120 },
+  },
+  {
+    id: 2,
+    title: "Mens Casual Premium Slim Fit T-Shirts ",
+    price: 22.3,
+    description:
+      "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+    category: "men's clothing",
+    image:
+      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+    rating: { rate: 4.1, count: 259 },
+  },
+  {
+    id: 3,
+    title: "Mens Cotton Jacket",
+    price: 55.99,
+    description:
+      "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+    rating: { rate: 4.7, count: 500 },
+  },
+  {
+    id: 4,
+    title: "Mens Casual Slim Fit",
+    price: 15.99,
+    description:
+      "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+    rating: { rate: 2.1, count: 430 },
+  },
+  {
+    id: 5,
+    title:
+      "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+    price: 695,
+    description:
+      "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+    category: "jewelery",
+    image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+    rating: { rate: 4.6, count: 400 },
+  },
+];
 
-  css.textContent = body.style.background + ";";
+function renderingCards(title, image, priceTag) {
+  const divCard = document.createElement("div");
+  divCard.className = "product-card";
+  productsDiv.append(divCard);
+
+  divCard.innerHTML = `<h2 class=".card-heading">${title}</h2><img class=".card-img" src="${image}"><p class=".card-price">${priceTag}</p>`;
+
+  // const h2 = document.createElement("h2");
+  // h2.className = ".card-heading";
+  // h2.textContent = title;
+  // const img = document.createElement("img");
+  // img.className = ".card-img";
+  // img.src = image;
+  // const price = document.createElement("p");
+  // price.className = ".card-price";
+  // price.textContent = priceTag;
+
+  // divCard.append(h2, img, price);
 }
 
-color1.addEventListener("input", setGradient);
-
-color2.addEventListener("input", setGradient);
+const renderingProductCards = productItemInfo.map((product) => {
+  renderingCards(product.title, product.image, product.price);
+  // const divCard = document.createElement("div");
+  // divCard.className = "product-card";
+  // productsDiv.append(divCard);
+  // const h2 = document.createElement("h2");
+  // h2.className = ".card-heading";
+  // h2.textContent = product.title;
+  // const img = document.createElement("img");
+  // img.className = ".card-img";
+  // img.src = product.img;
+  // const price = document.createElement("p");
+  // price.className = ".card-price";
+  // price.textContent = product.price;
+  // divCard.append(h2, img, price);
+});
