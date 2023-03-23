0a89e7e047abcb30034de931cbdee0dc16178de9
jumperM4 <pma1994ars@yandex.ru>
2023-03-23 22:08:10 +0300

    little optimisation

--- index.js
+++ index.js
@@ -16,31 +16,40 @@ function setGradient() {
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 
+//button
 const button = document.createElement("button");
 button.className = "color-generator";
 button.textContent = "Click Me!";
 body.append(button);
 
-button.addEventListener("click", () => {
+function setColor1() {
   let r1 = Math.round(Math.random() * 255.0);
   let g1 = Math.round(Math.random() * 255.0);
   let b1 = Math.round(Math.random() * 255.0);
   r1.toString(16);
   g1.toString(16);
   b1.toString(16);
+  let setColor1 = `#${r1.toString(16)}${g1.toString(16)}${b1.toString(16)}`;
+  return setColor1;
+}
 
+function setColor2() {
   let r2 = Math.round(Math.random() * 255.0);
   let g2 = Math.round(Math.random() * 255.0);
   let b2 = Math.round(Math.random() * 255.0);
   r2.toString(16);
   g2.toString(16);
   b2.toString(16);
-
-  let setColor1 = `#${r1.toString(16)}${g1.toString(16)}${b1.toString(16)}`;
   let setColor2 = `#${r2.toString(16)}${g2.toString(16)}${b2.toString(16)}`;
+  return setColor2;
+}
 
-  color1.value = setColor1;
-  color2.value = setColor2;
+button.addEventListener("click", () => {
+  const col1 = setColor1();
+  const col2 = setColor2();
+
+  color1.value = col1;
+  color2.value = col2;
 
   body.style.background =
     "linear-gradient(to right," + color1.value + ", " + color2.value + ")";
